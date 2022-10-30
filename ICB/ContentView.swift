49b4467175456c67394864a4d807b0763d84a6a2
Yeser Rodriguez <yeser112358@gmail.com>
2022-10-30 12:27:52 -0500

    Ciencias Basicas Escenario 1 - 2

---
+++ ICB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ICB
+//
+//  Created by Ana Milena Cañas Manrique on 10/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ICB/ICBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICBApp.swift
+//  ICB
+//
+//  Created by Ana Milena Cañas Manrique on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ICBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
